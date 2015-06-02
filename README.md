81cc84ce5f286485647b04bbb102c28568385cc9
Charles Lai <charles.lai@datadoghq.com>
2015-06-02 16:24:02 -0400

    Imrpove mcgrawtower twitter bot

---
+++ README.md
@@ -0,0 +1,6 @@
+# mcgrawtower
+Big Ben Clock clone for Cornell McGraw Tower
+
+This is the source code for @mcgrawtower. It is a simple Python script that tweets "CHIME" times the current hour. It used to be running on my RaspberryPI at home as a cron task schedueled to fire every hour. Now it is currently running on a DigitalOcean droplet.
+
+edit: this code is crap btw, im gonna rewrite this someday.
---
+++ main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+from twython import Twython
+import pytz
+import datetime
+import os
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+
+CONSUMER_KEY=os.environ.get('CONSUMER_KEY')
+CONSUMER_SECRET=os.environ.get('CONSUMER_SECRET')
+ACCESS_TOKEN=os.environ.get('ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET=os.environ.get('ACCESS_TOKEN_SECRET')
+
+
+sched = BlockingScheduler()
+
+
+# Update the Twitter account
+def tweet(msg):
+    # Authorize the account
+    twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    # Update the status and store result in callback
+    print CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+    print twitter.update_status(status=msg)
+
+
+@sched.scheduled_job('cron', minute=0)
+def main():
+    # Generate the status
+    tz = pytz.timezone("US/Eastern")
+    dt = datetime.datetime.now(tz)
+    hour = dt.hour % 12
+    if hour == 0:
+        hour = 12
+    msg = "CHIME " * hour
+    tweet(msg)
+
+sched.start()
